5c95a861932a59a23aee890ea0e565982b7ca5c0
eliasmg12345 <randyofgod666@gmail.com>
2026-04-03 22:55:11 -0400

    feat: implement JWT authentication with login and token verification

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,11 +2,39 @@ import { Injectable } from '@nestjs/common'
 import { RpcException } from '@nestjs/microservices'
 import { PrismaService } from 'src/prisma.service'
 import * as bcrypt from 'bcrypt'
-import { RegisterUserDto } from 'src/auth/dto'
+import { LoginUserDto, RegisterUserDto } from 'src/auth/dto'
+import { JwtService } from '@nestjs/jwt'
+import { JwtPayload } from './interfaces/jwt-payload.interface'
+import { envs } from 'src/config'
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async signJWT(paylodad: JwtPayload) {
+    return this.jwtService.sign(paylodad)
+  }
+
+  async verifyToken(token: string) {
+    try {
+      const { sub, iat, exp, ...user } = this.jwtService.verify(token, {
+        secret: envs.jwtSecret,
+      })
+      return {
+        user: user,
+        token: await this.signJWT(user),
+      }
+    } catch (error) {
+      console.log(error)
+      throw new RpcException({
+        status: 401,
+        message: 'Invalid Token',
+      })
+    }
+  }
 
   async registerUser(registerUserDto: RegisterUserDto) {
     const { email, name, password } = registerUserDto
@@ -30,10 +58,44 @@ export class AuthService {
         },
       })
 
-      const {password:__, ...rest} =newUser
+      const { password: __, ...rest } = newUser
       return {
         user: rest,
-        token: 'ABS',
+        token: await this.signJWT(rest),
+      }
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      })
+    }
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: { email },
+      })
+      if (!user) {
+        throw new RpcException({
+          status: 400,
+          message: 'User/Password not valid',
+        })
+      }
+      const isPasswordValid = bcrypt.compareSync(password, user.password)
+      if (!isPasswordValid) {
+        throw new RpcException({
+          status: 400,
+          message: 'User/Password not valid',
+        })
+      }
+
+      const { password: __, ...rest } = user
+
+      return {
+        user: rest,
+        token: await this.signJWT(rest),
       }
     } catch (error) {
       throw new RpcException({
--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,12 +5,14 @@ interface EnvVars {
   PORT: number
 
   NATS_SERVERS: string[]
+  JWT_SECRET: string
 }
 
 const envSchema = joi
   .object({
     PORT: joi.number().required(),
     NATS_SERVERS: joi.array().items(joi.string()).required(),
+    JWT_SECRET: joi.string().required(),
   })
   .unknown(true)
 
@@ -29,4 +31,5 @@ export const envs = {
   port: envVars.PORT,
 
   natsServers: envVars.NATS_SERVERS,
+  jwtSecret: envVars.JWT_SECRET,
 }
